442b87363b13b8d3d43cce825d855c7d3c53d4ed
Allohir <123628325+Allohir@users.noreply.github.com>
2023-02-06 23:50:42 +0400

    Add files via upload

---
+++ LearningProjects/AdministratorOfCafe/AdministratorOfCafe.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace AdministratorOfCafe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool IsOpen = true;
+
+            Table[] tables = { new Table(1, 4), new Table(2, 8), new Table(3, 10) };
+
+            while (IsOpen)
+            {
+                Console.WriteLine("Администрирование кафе.\n");
+                for (int i = 0; i < tables.Length; i++)
+                {
+                    tables[i].ShowInfo();
+                }
+
+                Console.Write("\nВведите номер стола: ");
+                int wishTable = Convert.ToInt32(Console.ReadLine()) - 1;
+                Console.Write("\nВведите количетсво мест для брони: ");
+                int desiredPlaces = Convert.ToInt32(Console.ReadLine());
+
+                bool isReservationCompleted = tables[wishTable].Reserve(desiredPlaces);
+                if (isReservationCompleted)
+                {
+                    Console.WriteLine("Бронь прошла успешно");
+                }
+                else
+                {
+                    Console.WriteLine("Бронь не прошла. Недостаточно мест");
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+
+            }
+
+        }
+    }
+
+    class Table
+    {
+        public int Number;
+        public int MaxPlaces;
+        public int FreePlaces;
+
+        public Table(int number, int maxPlaces)
+        {
+            Number = number;
+            MaxPlaces = maxPlaces;
+            FreePlaces = maxPlaces;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Стол: {Number}. Свободно мест: {FreePlaces} из {MaxPlaces}.");
+        }
+
+        public bool Reserve(int places)
+        {
+            if (FreePlaces >= places)
+            {
+                FreePlaces -= places;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ LearningProjects/Shop/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cart cart = new Cart();
+            cart.ShowProducts();
+
+            List<Product> products2 = new List<Product>();
+
+            for (int i = 0; i < cart.GetProductsCount(); i++)
+            {
+                products2.Add(cart.GetProductByIndex(i));
+            }
+
+            products2.RemoveAt(0);
+
+            Console.WriteLine();
+            cart.ShowProducts();
+            Console.WriteLine();
+
+            foreach (var product in products2)
+            {
+                Console.WriteLine(product.Name);
+            }
+        }
+    }
+
+    class Cart
+    {
+        private List<Product> products = new List<Product>();
+
+        public Cart()
+        {
+            products.Add(new Product("Яблоко"));
+            products.Add(new Product("Банан"));
+            products.Add(new Product("Апельсин"));
+            products.Add(new Product("Груша"));
+        }
+
+        public void ShowProducts()
+        {
+            foreach (var product in products)
+            {
+                Console.WriteLine(product.Name);
+            }
+        }
+
+        public Product GetProductByIndex(int index)
+        {
+            return products.ElementAt(index);
+        }
+
+        public int GetProductsCount()
+        {
+            return products.Count;
+        }
+    }
+
+    class Product
+    {
+        public string Name { get; private set; }
+
+        public Product(string name)
+        {
+            Name = name;
+        }
+    }
+}
